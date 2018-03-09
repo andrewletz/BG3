6bf4147e0486d8d0951d8b7cb0255dcd42f8c59f
Ethan Quick <efquick97@gmail.com>
2018-03-08 23:11:29 -0800

    forgot to add some files

---
+++ Game/src/engine/container.cpp
@@ -0,0 +1,45 @@
+#include <SFML/Graphics.hpp>
+#include <string>
+#include "container.hpp"
+
+
+Container::Container(Game* game, sf::Vector2f pos, std::string textureName)
+{
+	//game
+	this->game = game;
+	//texture
+	this->buttTexture = game->texmgr.getRef(textureName);
+	sf::Vector2u textureSize = this->buttTexture.getSize();
+	sf::Vector2f texSize;
+	texSize.x = textureSize.x;
+	texSize.y = textureSize.y;
+	//hitbox
+	this->hitbox.setPosition(pos);
+	this->hitbox.setSize(texSize);
+	this->hitbox.setOrigin(texSize.x/2, texSize.y/2);
+	this->hitbox.setTexture(&this->buttTexture);
+}
+
+void Container::updatePos(float scale)
+{
+	//hitbox size
+	sf::Vector2f hbs = this->hitbox.getSize();
+	float newHeight = hbs.x*scale;
+	float newWidth = hbs.y*scale;
+
+	hbs.x = newHeight;
+	hbs.y = newWidth;
+	this->hitbox.setSize(hbs);
+	this->hitbox.setOrigin(newHeight / 2, newWidth / 2);
+
+	//hitbox pos
+	sf::Vector2f hbp = this->hitbox.getPosition();
+	hbp.x *= scale;
+	hbp.y *= scale;
+	this->hitbox.setPosition(hbp);
+}
+
+void Container::Draw(sf::RenderWindow &window)
+{
+	window.draw(hitbox);
+}
---
+++ Game/src/engine/container.hpp
@@ -0,0 +1,22 @@
+#ifndef CONTAINER_HPP
+#define CONTAINER_HPP
+
+#include <SFML/Graphics.hpp>
+#include <string>
+#include "texture_manager.hpp"
+#include "game.hpp"
+
+class Container{
+public:
+	Container(Game* game, sf::Vector2f pos, std::string filename);
+	~Container(){}
+
+	void updatePos(float scale);
+	void Draw(sf::RenderWindow &window);
+
+	sf::RectangleShape hitbox;
+	sf::Texture buttTexture;
+	Game* game;
+};
+
+#endif
