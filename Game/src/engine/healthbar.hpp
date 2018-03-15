a22bbafa0db725c3369add805bbca6f341fff82b
Tristan Colby <tristan.colb@gmail.com>
2018-03-14 23:46:25 -0700

    added healthbar

--- Game/src/engine/healthbar.hpp
+++ Game/src/engine/healthbar.hpp
@@ -9,9 +9,10 @@ class HealthBar {
         sf::RectangleShape backBar;
         sf::RectangleShape healthBar;
 
+        int maxHP = 100;
         void draw(int curHP, sf::Vector2f newPos, sf::RenderWindow& window);
+        void setMax(int max);
     private:
-        int maxHP;
         sf::Vector2f maxSize;
 };
 
